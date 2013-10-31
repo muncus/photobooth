739117894f4c097601ca37c7fd4c21e02b0b7bba
Marc Dougherty <muncus@gmail.com>
2013-10-31 16:55:02 -0700

    reset device, and specify filename for downloaded images.

--- photobooth.sh
+++ photobooth.sh
@@ -12,7 +12,13 @@ while [[ 1 ]]; do
   read -n 1 char_read
   if [[ $char_read == $BUTTON_CHAR ]]; then
     echo "Taking photo."
-    gphoto2 --capture-image-and-download --hook-script ./uploadhook.sh
+    gphoto2 --capture-image-and-download --hook-script ./uploadhook.sh \
+      --filename "%Y%m%d%H%M%S.jpg"
+
+    # now reset the device, because the rpi's usb stack is a turd.
+    echo "resetting device."
+    buspath=`lsusb  | cut -f 2,4 -d\  --output-delimiter '/' | cut -f 1 -d :`
+    ./usbreset /dev/bus/usb/$buspath
     echo "done."
   fi
 done
